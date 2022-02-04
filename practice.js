89277a1d6489cf6147e70d461242ab8cc6876056
AhsanAliButt <60073494+AhsanAliButt@users.noreply.github.com>
2022-02-04 19:37:06 +0500

    update assignment 53-79

---
+++ practice.js
@@ -0,0 +1,43 @@
+let plan1={
+    name:"Plan1",
+    price:4000,
+    space:100,
+    data:1000,
+    pages:10,
+    discountMonths:[6,7]
+};
+console.log(`${typeof plan1}`)
+console.log(`${plan1.name}`);
+console.log(`${plan1.price}`);
+console.log(`${plan1.discountMonths[1]}`);
+console.log(plan1);
+
+// var myArray = [];
+// debugger
+// let i = 5;
+// while (i > 0) {
+//     myArray.push(i);
+//     i--;
+// }
+// console.log(myArray)
+// console.log(myArray)
+// const recordCollection = {
+//     2548: {
+//       albumTitle: 'Slippery When Wet',
+//       artist: 'Bon Jovi',
+//       tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//     },
+//     2468: {
+//       albumTitle: '1999',
+//       artist: 'Prince',
+//       tracks: ['1999', 'Little Red Corvette']
+//     },
+//     1245: {
+//       artist: 'Robert Palmer',
+//       tracks: []
+//     },
+//     5439: {
+//       albumTitle: 'ABBA Gold'
+//     }
+// };
+// console.log(recordCollection[2548]);
